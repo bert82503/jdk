8fc0f92b3a061f7c202c485bd0fc8a6316cd2b70
Bert Lee <bert825_work@163.com>
2016-08-30 17:48:14 +0800

    [SPI机制] ServiceLoader：服务加载器，一个简单的"服务-提供者"加载设施
    服务是接口和抽象类的集合，服务提供者是服务的特定实现。
    
    服务提供者是由在资源目录META-INF/services中放置提供者配置文件的标识。该文件的名称是服务类型的完全限定二进制名称，该文件包含一个具体的提供者类的完全限定二进制名称的列表，每行一个。
    
    提供者在需要时被延迟地定位和实例化(延迟迭代器)。
    服务加载器维护一个到目前为止已加载实例化的提供者的缓存。
    
    public void reload()：清除该加载器的提供者缓存，重新加载所有提供者(热加载机制)
    
    LazyIterator
    public boolean hasNext()：加载所有与该服务相关的提供者配置文件
    public S next()：实例化一个服务提供者对象，并缓存在本地(基于Class.newInstance()反射实现)
    
    public Iterator<S> iterator()：返回延迟地加载该服务的可用提供者的迭代器
    
    public static <S> ServiceLoader<S> load(Class<S> service, ClassLoader loader)：创建一个给定的服务类型和类加载器的服务加载器
    
    public static <S> ServiceLoader<S> load(Class<S> service)：创建一个给定的服务类型的服务加载器(使用当前线程上下文类加载)

---
+++ src/main/java/java/util/ServiceConfigurationError.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2005, 2006, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package java.util;
+
+
+/**
+ * Error thrown when something goes wrong while loading a service provider.
+ *
+ * <p/>
+ * 当加载服务提供者出现问题时，抛出的错误。
+ *
+ * <p> This error will be thrown in the following situations (在下列情况下将抛出此错误):
+ *
+ * <ul>
+ *
+ *   <li> The format of a provider-configuration file (提供者配置文件的格式) violates the <a
+ *   href="ServiceLoader.html#format">specification</a>; </li>
+ *
+ *   <li> An {@link java.io.IOException IOException} occurs while reading a
+ *   provider-configuration file (读取提供者配置文件时发生IO异常); </li>
+ *
+ *   <li> A concrete provider class named in a provider-configuration file
+ *   cannot be found (无法找到具体的提供者类); </li>
+ *
+ *   <li> A concrete provider class is not a subclass of the service class (具体的提供者类不是服务类的子类);
+ *   </li>
+ *
+ *   <li> A concrete provider class cannot be instantiated (具体的提供者类不能被实例化); or
+ *
+ *   <li> Some other kind of error occurs. </li>
+ *
+ * </ul>
+ *
+ *
+ * @author Mark Reinhold
+ * @since 1.6
+ */
+// [SPI机制] 服务配置错误
+public class ServiceConfigurationError
+    extends Error
+{
+
+    private static final long serialVersionUID = 74132770414881L;
+
+    /**
+     * Constructs a new instance with the specified message.
+     *
+     * @param  msg  The message, or <tt>null</tt> if there is no message
+     *
+     */
+    public ServiceConfigurationError(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs a new instance with the specified message and cause.
+     *
+     * @param  msg  The message, or <tt>null</tt> if there is no message
+     *
+     * @param  cause  The cause, or <tt>null</tt> if the cause is nonexistent
+     *                or unknown
+     */
+    public ServiceConfigurationError(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
