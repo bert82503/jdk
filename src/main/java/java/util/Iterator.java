ee1477941397ff279621be876eefe85a2f347b87
Bert Lee <bert825_work@163.com>
2016-09-10 23:25:20 +0800

    [遍历] Iterator<E>：集合上的迭代器，Iterable<T>：可迭代对象（允许对象为"foreach"语句的目标）

--- src/main/java/java/util/Iterator.java
+++ src/main/java/java/util/Iterator.java
@@ -29,19 +29,21 @@ package java.util;
  * An iterator over a collection.  {@code Iterator} takes the place of
  * {@link Enumeration} in the Java Collections Framework.  Iterators
  * differ from enumerations in two ways:
+ * 迭代器取代枚举。
+ * 迭代器与枚举的两个不同之处：
  *
  * <ul>
  *      <li> Iterators allow the caller to remove elements from the
  *           underlying collection during the iteration with well-defined
- *           semantics.
- *      <li> Method names have been improved.
+ *           semantics. (迭代器允许调用方在迭代期间从底层集合中删除元素)(具有明确定义的语义)
+ *      <li> Method names have been improved. (方法名称已得到改善)
  * </ul>
  *
  * <p>This interface is a member of the
  * <a href="{@docRoot}/../technotes/guides/collections/index.html">
  * Java Collections Framework</a>.
  *
- * @param <E> the type of elements returned by this iterator
+ * @param <E> the type of elements returned by this iterator (这个迭代器返回的元素类型)
  *
  * @author  Josh Bloch
  * @see Collection
@@ -49,12 +51,14 @@ package java.util;
  * @see Iterable
  * @since 1.2
  */
-
+// 核心接口 [遍历] 集合上的迭代器
 public interface Iterator<E> {
     /**
      * Returns {@code true} if the iteration has more elements.
      * (In other words, returns {@code true} if {@link #next} would
      * return an element rather than throwing an exception.)
+     * <p>
+     * 如果迭代中还有元素，则返回{@code true}。
      *
      * @return {@code true} if the iteration has more elements
      */
@@ -62,6 +66,8 @@ public interface Iterator<E> {
 
     /**
      * Returns the next element in the iteration.
+     * <p>
+     * 返回迭代中的下一个元素。
      *
      * @return the next element in the iteration
      * @throws NoSuchElementException if the iteration has no more elements
@@ -75,9 +81,11 @@ public interface Iterator<E> {
      * is unspecified if the underlying collection is modified while the
      * iteration is in progress in any way other than by calling this
      * method.
+     * <p>
+     * 从底层集合中移除此迭代器返回的最后一个元素（可选操作）。
      *
      * @throws UnsupportedOperationException if the {@code remove}
-     *         operation is not supported by this iterator
+     *         operation is not supported by this iterator (不支持操作异常)
      *
      * @throws IllegalStateException if the {@code next} method has not
      *         yet been called, or the {@code remove} method has already
