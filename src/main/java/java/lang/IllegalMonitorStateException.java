4d9aef7faca41b8552689596eadee9ec00a4ce4a
Edward Lee <bert825_work@163.com>
2017-01-02 15:50:58 +0800

    [lang] Object中涉及的相关异常（IllegalArgumentException、IllegalMonitorStateException、InterruptedException）

---
+++ src/main/java/java/lang/IllegalMonitorStateException.java
@@ -0,0 +1,40 @@
+package java.lang;
+
+/**
+ * Thrown to indicate that a thread has attempted to wait on an
+ * object's monitor or to notify other threads waiting on an object's
+ * monitor without owning the specified monitor.
+ * <p>
+ * 非法监视器状态异常：表示线程已尝试等待对象的监视器或者通知正在等待对象的监视器的其他线程，
+ * 而不是拥有指定的监视器。
+ *
+ * @author  unascribed
+ * @see     java.lang.Object#notify()
+ * @see     java.lang.Object#notifyAll()
+ * @see     java.lang.Object#wait()
+ * @see     java.lang.Object#wait(long)
+ * @see     java.lang.Object#wait(long, int)
+ * @since   JDK1.0
+ */
+public
+class IllegalMonitorStateException extends RuntimeException {
+    private static final long serialVersionUID = 3713306369498869069L;
+
+    /**
+     * Constructs an <code>IllegalMonitorStateException</code> with no
+     * detail message.
+     */
+    public IllegalMonitorStateException() {
+        super();
+    }
+
+    /**
+     * Constructs an <code>IllegalMonitorStateException</code> with the
+     * specified detail message.
+     *
+     * @param   s   the detail message.
+     */
+    public IllegalMonitorStateException(String s) {
+        super(s);
+    }
+}
