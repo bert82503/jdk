24bf1fed2638922f380977b78effbcdae23ef38b
Edward Lee <bert825_work@163.com>
2017-06-04 17:26:38 +0800

    [函数式接口-Function] 基本类型的结果(ToIntFunction<R>、ToLongFunction<R>、ToDoubleFunction<R>)

--- src/main/java/java/util/function/ToIntFunction.java
+++ src/main/java/java/util/function/ToIntFunction.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 package java.util.function;
 
 /**
@@ -36,6 +12,7 @@ package java.util.function;
  * @see Function
  * @since 1.8
  */
+// 从T到int的一元函数
 @FunctionalInterface
 public interface ToIntFunction<T> {
 
