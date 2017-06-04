3ef303a478c23f70f7b7ffe24346d2f494a591cd
Edward Lee <bert825_work@163.com>
2017-06-04 18:49:14 +0800

    [函数式接口-BinaryOperator] 基本类型的二元运算符(IntBinaryOperator、LongBinaryOperator、DoubleBinaryOperator)

--- src/main/java/java/util/function/IntBinaryOperator.java
+++ src/main/java/java/util/function/IntBinaryOperator.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 package java.util.function;
 
 /**
@@ -36,6 +12,7 @@ package java.util.function;
  * @see IntUnaryOperator
  * @since 1.8
  */
+// 二元整数运算符
 @FunctionalInterface
 public interface IntBinaryOperator {
 
