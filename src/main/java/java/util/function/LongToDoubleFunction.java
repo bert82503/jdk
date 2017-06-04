2cf5bee6a06fbe01e20a972a62ab73f7e027d1e6
Edward Lee <bert825_work@163.com>
2017-06-04 17:32:56 +0800

    [函数式接口-Function] 基本类型的参数和结果(IntToLongFunction、IntToDoubleFunction、...)

--- src/main/java/java/util/function/LongToDoubleFunction.java
+++ src/main/java/java/util/function/LongToDoubleFunction.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 package java.util.function;
 
 /**
@@ -35,6 +11,7 @@ package java.util.function;
  * @see Function
  * @since 1.8
  */
+// 从long到double的一元函数
 @FunctionalInterface
 public interface LongToDoubleFunction {
 
