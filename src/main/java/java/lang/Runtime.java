47162447de3dc2386c8fb8041a0d8eb33066a733
edwardlee03 <bert825_work@163.com>
2022-04-02 17:01:06 +0800

    improve the comment of Runtime

--- src/main/java/java/lang/Runtime.java
+++ src/main/java/java/lang/Runtime.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 1995, 2006, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 
 package java.lang;
 
@@ -36,9 +12,9 @@ import sun.reflect.Reflection;
  * <code>Runtime</code> that allows the application to interface with
  * the environment in which the application is running. The current
  * runtime can be obtained from the <code>getRuntime</code> method.
- * <p/>
  * 每个Java应用程序都有一个单独的运行时类的实例，允许应用程序与应用程序运行的环境进行交互。
  * 从 {@link #getRuntime()} 方法可以获得当前的运行时环境。
+ * 每个Java应用程序都有一个单独的运行时类的实例(运行时环境)。
  * <p/>
  * An application cannot create its own instance of this class.
  *
@@ -46,10 +22,9 @@ import sun.reflect.Reflection;
  * @see java.lang.Runtime#getRuntime()
  * @since JDK1.0
  */
-// 核心类 每个Java应用程序都有一个单独的运行时类的实例(运行时环境)
 public class Runtime {
     /**
-     * 当前的运行时环境(单实例)
+     * 当前的运行时环境(单个实例)
      */
     private static final Runtime currentRuntime = new Runtime();
 
@@ -57,13 +32,11 @@ public class Runtime {
      * Returns the runtime object associated with the current Java application.
      * Most of the methods of class <code>Runtime</code> are instance
      * methods and must be invoked with respect to the current runtime object.
-     * <p>
      * 返回与当前Java应用程序关联的运行时对象。
      *
      * @return the <code>Runtime</code> object associated with the current
      * Java application.
      */
-    // 核心方法 返回与当前Java应用程序相关的运行时对象
     public static Runtime getRuntime() {
         return currentRuntime;
     }
@@ -75,12 +48,12 @@ public class Runtime {
     }
 
     // 关闭虚拟机
+
     /**
      * Terminates the currently running Java virtual machine by initiating its
      * shutdown sequence.  This method never returns normally.  The argument
      * serves as a status code; by convention, a nonzero status code indicates
      * abnormal termination.
-     * <p>
      * 通过启动关闭程序来终止当前正在运行的Java虚拟机。
      * 参数用作状态代码，按照惯例，非零状态代码表示异常终止。
      * <p> The virtual machine's shutdown sequence consists of two phases.  In
@@ -88,7 +61,6 @@ public class Runtime {
      * if any, are started in some unspecified order and allowed to run
      * concurrently until they finish.  Once this is done the virtual machine {@link #halt
      * halts}.
-     * <p>
      * 虚拟机的关闭程序由两个阶段组成：
      * 在第一阶段，所有注册的关闭挂钩({@link #addShutdownHook})以一些未指定的顺序启动并允许并发地运行，直到它们完成。
      * 一旦完成，虚拟机就停止了({@link #halt})。
@@ -98,7 +70,6 @@ public class Runtime {
      * finalization has been enabled then this method halts the virtual machine
      * with the given status code if the status is nonzero; otherwise, it
      * blocks indefinitely.
-     * <p>
      * 如果此方法在虚拟机已开始关闭程序后被调用，那么正在运行此方法的关闭挂钩将无限期地阻塞。
      * 如果关闭挂钩已运行和退出确定已启用，则此方法使用给定的非零状态代码来停止虚拟机；
      * 否则，它将无限期地阻塞。
@@ -118,18 +89,18 @@ public class Runtime {
      * @see #removeShutdownHook
      * @see #halt(int)
      */
-    // 核心方法 通过启动关闭程序来终止当前正在运行的Java虚拟机
     public void exit(int status) {
-        SecurityManager security = System.getSecurityManager(); // 安全策略管理器
+        // 安全策略管理器
+        SecurityManager security = System.getSecurityManager();
         if (security != null) {
             security.checkExit(status);
         }
-        Shutdown.exit(status); // 退出虚拟机
+        // 退出虚拟机
+        Shutdown.exit(status);
     }
 
     /**
      * Registers a new virtual-machine shutdown hook.
-     * <p>
      * 注册一个新的虚拟机关闭挂钩。
      * <p> The Java virtual machine <i>shuts down</i> in response to two kinds
      * of events:
@@ -152,7 +123,6 @@ public class Runtime {
      * continue to run during the shutdown sequence, as will non-daemon threads
      * if shutdown was initiated by invoking the <tt>{@link #exit exit}</tt>
      * method.
-     * <p>
      * 关闭钩子是一个已初始化但未开始的线程。
      * 当虚拟机开始其关闭程序时，它将以一些未指定的顺序开始所有注册的关闭挂钩，并让它们并发地运行。
      * 当所有的关闭挂钩都已经完成，如果在退出时撤出(finalization-on-exit)已经启用，然后它会运行所有未运行的终结器。
@@ -161,13 +131,11 @@ public class Runtime {
      * <p> Once the shutdown sequence has begun it can be stopped only by
      * invoking the <tt>{@link #halt halt}</tt> method, which forcibly
      * terminates the virtual machine.
-     * <p>
      * 一旦关闭程序开始，仅能通过调用 <tt>{@link #halt}</tt> 方法来停止，它将强制终止虚拟机。
      * <p> Once the shutdown sequence has begun it is impossible to register a
      * new shutdown hook or de-register a previously-registered hook.
      * Attempting either of these operations will cause an
      * <tt>{@link IllegalStateException}</tt> to be thrown.
-     * <p>
      * 一旦关闭程序开始，关闭挂钩就不可变更。
      * <p> Shutdown hooks run at a delicate time in the life cycle of a virtual
      * machine and should therefore be coded defensively.  They should, in
@@ -177,7 +145,6 @@ public class Runtime {
      * the process of shutting down.  Attempts to use other thread-based
      * services such as the AWT event-dispatch thread, for example, may lead to
      * deadlocks.
-     * <p>
      * 关闭挂钩运行在虚拟机的生命周期中的一个微妙的时间，因此应进行防御式编程。
      * 它们应该是线程安全的，以尽可能地避免死锁。
      * 它们也不应盲目地依赖那些可以注册自己的关闭程序的服务，可能它们就位于关闭程序的过程中。
@@ -190,7 +157,6 @@ public class Runtime {
      * which to shut down and exit.  It is therefore inadvisable to attempt any
      * user interaction or to perform a long-running computation in a shutdown
      * hook.
-     * <p>
      * 关闭挂钩应该迅速完成其工作。
      * 当程序调用 <tt>{@link #exit}</tt> 方法时，期望是虚拟机将立即关闭并退出。
      * <p> Uncaught exceptions are handled in shutdown hooks just as in any
@@ -200,7 +166,6 @@ public class Runtime {
      * prints the exception's stack trace to <tt>{@link System#err}</tt> and
      * terminates the thread; it does not cause the virtual machine to exit or
      * halt.
-     * <p>
      * 就像在任何其它线程中那样，在关闭挂钩中处理未捕获的异常，
      * 通过调用线程组对象的 <tt>{@link ThreadGroup#uncaughtException}</tt> 方法。
      * 此方法的默认实现是打印异常的堆栈跟踪到 <tt>{@link System#err}</tt> 并终止线程，
@@ -214,7 +179,6 @@ public class Runtime {
      * attempting to access nonexistent memory.  If the virtual machine aborts
      * then no guarantee can be made about whether or not any shutdown hooks
      * will be run.
-     * <p>
      * 在极少数情况下，虚拟机可能中止，即停止运行而不是正常关闭。
      * 虚拟机也可能终止，如果一个本地方法出错了，如破坏内部数据结构或试图访问不存在的内存。
      * 如果虚拟机中止，是否将运行任何关闭挂钩，不能做任何保证。
@@ -232,18 +196,17 @@ public class Runtime {
      * @see #exit(int)
      * @since 1.3
      */
-    // 核心方法 注册一个新的虚拟机关闭挂钩
     public void addShutdownHook(Thread hook) {
         SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new RuntimePermission("shutdownHooks"));
         }
-        ApplicationShutdownHooks.add(hook); // 添加应用程序的关闭挂钩
+        // 添加应用程序的关闭挂钩
+        ApplicationShutdownHooks.add(hook);
     }
 
     /**
-     * De-registers a previously-registered virtual-machine shutdown hook. <p>
-     *
+     * De-registers a previously-registered virtual-machine shutdown hook.
      * 注销一个以前已注册的虚拟机关闭挂钩。
      *
      * @param hook the hook to remove (要移除的关闭挂钩)
@@ -258,19 +221,18 @@ public class Runtime {
      * @see #exit(int)
      * @since 1.3
      */
-    // 核心方法 注销一个以前已注册的虚拟机关闭挂钩
     public boolean removeShutdownHook(Thread hook) {
         SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new RuntimePermission("shutdownHooks"));
         }
-        return ApplicationShutdownHooks.remove(hook); // 移除应用程序的关闭挂钩
+        // 移除应用程序的关闭挂钩
+        return ApplicationShutdownHooks.remove(hook);
     }
 
     /**
      * Forcibly terminates the currently running Java virtual machine.  This
      * method never returns normally.
-     * <p>
      * 强行终止当前正在运行的Java虚拟机。
      * <p> This method should be used with extreme caution.  Unlike the
      * <tt>{@link #exit exit}</tt> method, this method does not cause shutdown
@@ -278,7 +240,6 @@ public class Runtime {
      * finalization-on-exit has been enabled.  If the shutdown sequence has
      * already been initiated then this method does not wait for any running
      * shutdown hooks or finalizers to finish their work.
-     * <p>
      * 此方法应慎用。
      * 不像 <tt>{@link #exit}</tt> 方法，此方法不会触发关闭挂钩开始运行，也不会运行未调用的终结器。
      * 如果关闭程序已经启动，此方法不会等待任何运行中的关闭挂钩或终结器来完成它们的工作。
@@ -301,14 +262,15 @@ public class Runtime {
         if (sm != null) {
             sm.checkExit(status);
         }
-        Shutdown.halt(status); // 终止虚拟机
+        // 终止虚拟机
+        Shutdown.halt(status);
     }
 
 
     // 在进程中执行系统命令
+
     /**
      * Executes the specified string command in a separate process.
-     * <p>
      * 在单独的进程中执行指定的字符串命令。
      * <p>This is a convenience method.  An invocation of the form
      * <tt>exec(command)</tt>
@@ -400,13 +362,15 @@ public class Runtime {
      */
     public Process exec(String command, String[] envp, File dir)
             throws IOException {
-        if (command.length() == 0)
+        if (command.length() == 0) {
             throw new IllegalArgumentException("Empty command");
+        }
 
         StringTokenizer st = new StringTokenizer(command);
         String[] cmdarray = new String[st.countTokens()];
-        for (int i = 0; st.hasMoreTokens(); i++)
+        for (int i = 0; st.hasMoreTokens(); i++) {
             cmdarray[i] = st.nextToken();
+        }
         return exec(cmdarray, envp, dir);
     }
 
@@ -540,6 +504,7 @@ public class Runtime {
      */
     public Process exec(String[] cmdarray, String[] envp, File dir)
             throws IOException {
+        // 进程启动
         return new ProcessBuilder(cmdarray)
                 .environment(envp)
                 .directory(dir)
@@ -563,13 +528,13 @@ public class Runtime {
     public native int availableProcessors();
 
 
-    // JVM运行时状态（监控）
+    // JVM运行时状态(监控)
+
     /**
      * Returns the amount of free memory in the Java Virtual Machine.
      * Calling the
      * <code>gc</code> method may result in increasing the value returned
      * by <code>freeMemory.</code>
-     * <p>
      * 返回Java虚拟机中可用内存的数量。
      * 调用 {@link #gc()} 方法可能会导致返回的值增加
      *
@@ -582,7 +547,6 @@ public class Runtime {
      * Returns the total amount of memory in the Java virtual machine.
      * The value returned by this method may vary over time, depending on
      * the host environment.
-     * <p>
      * 返回Java虚拟机中的内存总量。
      * <p/>
      * Note that the amount of memory required to hold an object of any
@@ -613,7 +577,6 @@ public class Runtime {
      * they currently occupy available for quick reuse. When control
      * returns from the method call, the virtual machine has made
      * its best effort to recycle all discarded objects.
-     * <p>
      * 运行垃圾收集器。
      * <p/>
      * The name <code>gc</code> stands for "garbage
@@ -695,7 +658,6 @@ public class Runtime {
      * argument must be a complete path name,
      * (for example
      * <code>Runtime.getRuntime().load("/home/avh/lib/libX11.so");</code>).
-     * <p>
      * 加载指定的文件名作为动态库。
      * <p/>
      * First, if there is a security manager, its <code>checkLink</code>
