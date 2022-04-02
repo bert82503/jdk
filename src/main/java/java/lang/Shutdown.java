69cc24c6c0615015df4821fc93e782c81bbf5c78
edwardlee03 <bert825_work@163.com>
2022-04-02 18:02:49 +0800

    improve comment of Shutdown

--- src/main/java/java/lang/Shutdown.java
+++ src/main/java/java/lang/Shutdown.java
@@ -1,49 +1,34 @@
-/*
- * Copyright (c) 1999, 2005, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 
 package java.lang;
 
 /**
  * Package-private utility class containing data structures and logic
  * governing the virtual-machine shutdown sequence.
- * <p>
- * 包含数据结构和逻辑的虚拟机关闭程序的辅助类
+ * [虚拟机关闭程序] 包含数据结构和逻辑的虚拟机关闭程序的辅助类。
  *
  * @author   Mark Reinhold
  * @since    1.3
  */
-// [虚拟机关闭程序] 包含数据结构和逻辑的虚拟机关闭程序的辅助类
 class Shutdown {
 
-    /* Shutdown state (关闭状态) */
-    private static final int RUNNING = 0; // 运行中
-    private static final int HOOKS = 1; // 挂钩执行中
-    private static final int FINALIZERS = 2; // 终结器执行中
+    /**
+     * 运行中
+     */
+    private static final int RUNNING = 0;
+    /**
+     * 挂钩执行中
+     */
+    private static final int HOOKS = 1;
+    /**
+     * 终结器执行中
+     */
+    private static final int FINALIZERS = 2;
+    /**
+     * Shutdown state (关闭状态)
+     */
     private static int state = RUNNING;
 
-    /* Should we run all finalizers upon exit? (要在退出时运行所有终结器吗？) */
+    /** Should we run all finalizers upon exit? (要在退出时运行所有终结器吗？) */
     private static boolean runFinalizersOnExit = false;
 
     // The system shutdown hooks are registered with a predefined slot. (使用预定义的插槽来注册系统的关闭挂钩)
@@ -51,20 +36,26 @@ class Shutdown {
     // (0) Console restore hook (控制台恢复挂钩)
     // (1) Application hooks (应用程序挂钩)
     // (2) DeleteOnExit hook (在退出时删除挂钩)
-    private static final int MAX_SYSTEM_HOOKS = 10; // 最大系统挂钩数量
-    private static final Runnable[] hooks = new Runnable[MAX_SYSTEM_HOOKS]; // 数组+列表
+    /**
+     * 最大系统挂钩数量
+     */
+    private static final int MAX_SYSTEM_HOOKS = 10;
+    /**
+     * 数组+列表
+     */
+    private static final Runnable[] hooks = new Runnable[MAX_SYSTEM_HOOKS];
 
     // the index of the currently running shutdown hook to the hooks array (挂钩数组中当前运行的关闭挂钩所在的索引)
     private static int currentRunningHook = 0;
 
-    /* The preceding static fields are protected by this lock (上述静态字段被这把锁保护) */
+    /** The preceding static fields are protected by this lock (上述静态字段被这把锁保护) */
     private static class Lock { }
     private static Object lock = new Lock(); // 锁对象
 
-    /* Lock object for the native halt method (本地终止方法的锁对象) */
+    /** Lock object for the native halt method (本地终止方法的锁对象) */
     private static Object haltLock = new Lock();
 
-    /* Invoked by Runtime.runFinalizersOnExit */
+    /** Invoked by Runtime.runFinalizersOnExit */
     static void setRunFinalizersOnExit(boolean run) {
         synchronized (lock) {
             runFinalizersOnExit = run;
@@ -75,7 +66,6 @@ class Shutdown {
     /**
      * Add a new shutdown hook.  Checks the shutdown state and the hook itself,
      * but does not do any security checks.
-     * <p>
      * 添加一个新的关闭挂钩。
      * 检查关闭状态和挂钩本身，但不做任何安全检查。
      *
@@ -95,52 +85,62 @@ class Shutdown {
      *        if registerShutdownInProgress is true and the shutdown process
      *           already passes the given slot
      */
-    // 核心方法 添加一个新的关闭挂钩
     static void add(int slot, boolean registerShutdownInProgress, Runnable hook) {
-        synchronized (lock) { // 锁对象监视器同步语句
-            if (hooks[slot] != null)
+        // 锁对象监视器同步语句
+        synchronized (lock) {
+            if (hooks[slot] != null) {
                 throw new InternalError("Shutdown hook at slot " + slot + " already registered"); // 关闭钩槽已经注册过
-
-            if (!registerShutdownInProgress) {
-                if (state > RUNNING)
-                    throw new IllegalStateException("Shutdown in progress"); // 正在关闭中
-            } else {
-                if (state > HOOKS || (state == HOOKS && slot <= currentRunningHook))
-                    throw new IllegalStateException("Shutdown in progress");
             }
 
-            hooks[slot] = hook; // 保存到对应的挂钩数组槽中
+            if (!registerShutdownInProgress) {
+                // 正在关闭中
+                if (state > RUNNING) {
+                    throw new IllegalStateException("Shutdown in progress");
+                }
+            } else {
+                if (state > HOOKS || (state == HOOKS && slot <= currentRunningHook)) {
+                    throw new IllegalStateException("Shutdown in progress");
+                }
+            }
+
+            // 保存到对应的挂钩数组槽中
+            hooks[slot] = hook;
         }
     }
 
-    /*
-     * Run all registered shutdown hooks
+    /**
+     * Run all registered shutdown hooks.
+     * 运行所有注册的关闭挂钩。
      */
-    // 核心实现 运行所有注册的关闭挂钩
     private static void runHooks() {
         for (int i = 0; i < MAX_SYSTEM_HOOKS; i++) {
             try {
-                Runnable hook; // 每次循环都新定义局部变量
-                synchronized (lock) { // 同步
+                // 每次循环都新定义局部变量
+                Runnable hook;
+                // 同步
+                synchronized (lock) {
                     // acquire the lock to make sure the hook registered during
                     // shutdown is visible here.
                     // 获取锁以确保注册的挂钩在关闭期间是可见的
                     currentRunningHook = i;
-                    hook = hooks[i]; // 挂钩
+                    // 挂钩
+                    hook = hooks[i];
                 }
                 if (hook != null) {
-                    hook.run(); // 执行关闭挂钩线程
+                    // 执行关闭挂钩线程
+                    hook.run();
                 }
             } catch(Throwable t) {
                 if (t instanceof ThreadDeath) {
-                    ThreadDeath td = (ThreadDeath) t; // 线程停止
+                    // 线程停止
+                    ThreadDeath td = (ThreadDeath) t;
                     throw td;
                 }
             }
         }
     }
 
-    /* The halt method is synchronized on the halt lock
+    /** The halt method is synchronized on the halt lock
      * to avoid corruption of the delete-on-shutdown file list.
      * It invokes the true native halt method.
      * 终止方法是在终止锁进行同步，以避免在关闭时删除的文件列表的损坏。
@@ -154,11 +154,11 @@ class Shutdown {
 
     static native void halt0(int status);
 
-    /* Wormhole for invoking java.lang.ref.Finalizer.runAllFinalizers (蠕虫攻击) */
+    /** Wormhole for invoking java.lang.ref.Finalizer.runAllFinalizers (蠕虫攻击) */
     private static native void runAllFinalizers();
 
 
-    /*
+    /**
      * The actual shutdown sequence is defined here.
      * 这里定义实际的关闭程序。
      *
@@ -171,41 +171,53 @@ class Shutdown {
      * initiated by an exit(0); they're never run on exit(n) for n != 0 or in
      * response to SIGINT, SIGTERM, etc.
      */
-    // 核心实现 定义实际的关闭程序
     private static void sequence() {
         synchronized (lock) {
             /* Guard against the possibility of a daemon thread invoking exit
              * after DestroyJavaVM initiates the shutdown sequence
              * 警惕守护线程在销毁的 JVM 启动关闭程序之后调用退出虚拟机的可能性
              */
-            if (state != HOOKS) return;
+            if (state != HOOKS) {
+                return;
+            }
         }
-        runHooks(); // 运行所有注册的关闭挂钩
+        // 运行所有注册的关闭挂钩
+        runHooks();
         boolean rfoe;
         synchronized (lock) {
-            state = FINALIZERS; // 更新到终结器执行状态
+            // 更新到终结器执行状态
+            state = FINALIZERS;
             rfoe = runFinalizersOnExit;
         }
-        if (rfoe) runAllFinalizers(); // 运行所有的终结器
+        if (rfoe) {
+            // 运行所有的终结器
+            runAllFinalizers();
+        }
     }
 
 
-    /* Invoked by Runtime.exit, which does all the security checks.
+    /** Invoked by Runtime.exit, which does all the security checks.
      * Also invoked by handlers for system-provided termination events,
      * which should pass a nonzero status code.
      * 被 Runtime.exit 方法调用，它会做所有的安全检查。
      * 也可以被系统提供的终止事件的处理程序调用，它应该会传入一个非零的状态代码。
+     * 退出虚拟机，被Runtime.exit方法调用，它会做所有的安全检查。
      */
-    // 核心方法 退出虚拟机，被Runtime.exit方法调用，它会做所有的安全检查
     static void exit(int status) {
         boolean runMoreFinalizers = false;
-        synchronized (lock) { // 锁对象监视器同步语句
-            if (status != 0) runFinalizersOnExit = false; // 非正常退出
+        // 锁对象监视器同步语句
+        synchronized (lock) {
+            if (status != 0) {
+                // 非正常退出
+                runFinalizersOnExit = false;
+            }
             switch (state) {
-            case RUNNING:       /* Initiate shutdown (启动关闭程序) */
+            case RUNNING:
+                /* Initiate shutdown (启动关闭程序) */
                 state = HOOKS;
                 break;
-            case HOOKS:         /* Stall and halt (拖延并终止) */
+            case HOOKS:
+                /* Stall and halt (拖延并终止) */
                 break;
             case FINALIZERS:
                 if (status != 0) {
@@ -220,22 +232,26 @@ class Shutdown {
                 break;
             }
         }
-        if (runMoreFinalizers) { // 运行终结器
+        if (runMoreFinalizers) {
+            // 运行终结器
             runAllFinalizers();
             halt(status);
         }
-        synchronized (Shutdown.class) { // 类对象监视器同步语句，保证JVM内互斥
+        // 类对象监视器同步语句，保证JVM内互斥
+        synchronized (Shutdown.class) {
             /* Synchronize on the class object, causing any other thread
              * that attempts to initiate shutdown to stall indefinitely
              * 在类对象上进行同步，造成任何其他线程尝试启动关闭程序都会无限期地拖延
              */
-            sequence(); // 执行关闭程序
-            halt(status); // 停止虚拟机
+            // 执行关闭程序
+            sequence();
+            // 停止虚拟机
+            halt(status);
         }
     }
 
 
-    /* Invoked by the JNI DestroyJavaVM procedure when the last non-daemon
+    /** Invoked by the JNI DestroyJavaVM procedure when the last non-daemon
      * thread has finished.  Unlike the exit method, this method does not
      * actually halt the VM.
      * 在最后一个非守护线程完成时，由 JNI 销毁的 JVM 过程调用。
@@ -244,17 +260,20 @@ class Shutdown {
     static void shutdown() {
         synchronized (lock) {
             switch (state) {
-            case RUNNING:       /* Initiate shutdown */
+            case RUNNING:
+                /* Initiate shutdown */
                 state = HOOKS;
                 break;
-            case HOOKS:         /* Stall and then return */
+            case HOOKS:
+                /* Stall and then return */
             case FINALIZERS:
                 break;
             }
         }
-        synchronized (Shutdown.class) { // 类对象监视器同步语句，保证JVM内互斥
-            sequence(); // 执行关闭程序
+        // 类对象监视器同步语句，保证JVM内互斥
+        synchronized (Shutdown.class) {
+            // 执行关闭程序
+            sequence();
         }
     }
-
 }
--- src/main/java/java/lang/Terminator.java
+++ src/main/java/java/lang/Terminator.java
@@ -1,38 +1,12 @@
-/*
- * Copyright (c) 1999, 2001, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 
 package java.lang;
 
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
-
 /**
  * Package-private utility class for setting up and tearing down
  * platform-specific support for termination-triggered shutdowns.
- * <p>
  * 安装和拆除支持终止触发关闭特定平台的辅助类。
  *
  * @author   Mark Reinhold
@@ -40,21 +14,26 @@ import sun.misc.SignalHandler;
  */
 class Terminator {
 
-    // 信号处理程序
+    /**
+     * 信号处理程序
+     */
     private static SignalHandler handler = null;
 
-    /* Invocations of setup and tear down are already synchronized
+    /** Invocations of setup and tear down are already synchronized
      * on the shutdown lock, so no further synchronization is needed here
-     * 安装和拆除的调用是基于关闭锁同步的
+     * 安装和拆除的调用是基于关闭锁同步的。
+     * 设置Linux软中断信号处理程序(HUP：终端挂起、INT：键盘中断、TERM：终止)。
      */
-    // 核心方法 设置Linux软中断信号处理程序(HUP：终端挂起、INT：键盘中断、TERM：终止)
     static void setup() {
         // 初始化信号处理程序
-        if (handler != null) return;
+        if (handler != null) {
+            return;
+        }
         SignalHandler sh = new SignalHandler() {
             @Override
             public void handle(Signal sig) {
-                Shutdown.exit(sig.getNumber() + 0200); // 退出虚拟机
+                // 退出虚拟机
+                Shutdown.exit(sig.getNumber() + 0200);
             }
         };
         handler = sh;
@@ -64,17 +43,20 @@ class Terminator {
         // System.exit()
         // 当 -Xrs 参数被指定时，用户负责确保通过调用 System.exit() 来运行关闭挂钩
         try {
-            Signal.handle(new Signal("HUP"), sh); // 终端挂起或者控制进程终止
+            // 终端挂起或者控制进程终止
+            Signal.handle(new Signal("HUP"), sh);
         } catch (IllegalArgumentException e) {
             // 忽略异常
         }
         try {
-            Signal.handle(new Signal("INT"), sh); // 键盘中断（同 Ctrl+C）
+            // 键盘中断（同 Ctrl+C）
+            Signal.handle(new Signal("INT"), sh);
         } catch (IllegalArgumentException e) {
             // 忽略异常
         }
         try {
-            Signal.handle(new Signal("TERM"), sh); // 终止信号
+            // 终止信号
+            Signal.handle(new Signal("TERM"), sh);
         } catch (IllegalArgumentException e) {
             // 忽略异常
         }
