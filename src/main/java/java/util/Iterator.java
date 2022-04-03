5ee2ab8c00a8626dc12c77bf3fba77b86d58f163
edwardlee03 <bert825_work@163.com>
2022-04-03 21:06:26 +0800

    improve comment of Collection<E> 元素集合与数据流的桥接

--- src/main/java/java/util/Iterator.java
+++ src/main/java/java/util/Iterator.java
@@ -2,24 +2,24 @@
 package java.util;
 
 /**
+ * 集合上的迭代器，迭代器取代枚举遍历。
  * An iterator over a collection.  {@code Iterator} takes the place of
  * {@link Enumeration} in the Java Collections Framework.  Iterators
  * differ from enumerations in two ways:
- * 集合上的迭代器，迭代器取代枚举。
  * 迭代器与枚举的两个不同之处：
  *
  * <ul>
  *      <li> Iterators allow the caller to remove elements from the
  *           underlying collection during the iteration with well-defined
- *           semantics. (迭代器允许调用方在迭代期间从底层集合中删除元素)(具有明确定义的语义)
- *      <li> Method names have been improved. (方法名称已得到改善)
+ *           semantics. 迭代器允许调用方在迭代期间从底层集合中删除元素，具有明确定义的语义
+ *      <li> Method names have been improved. 方法名称已得到改善
  * </ul>
  *
  * <p>This interface is a member of the
  * <a href="{@docRoot}/../technotes/guides/collections/index.html">
  * Java Collections Framework</a>.
  *
- * @param <E> the type of elements returned by this iterator (这个迭代器返回的元素类型)
+ * @param <E> the type of elements returned by this iterator 这个迭代器返回的元素类型
  *
  * @author  Josh Bloch
  * @see Collection
@@ -28,12 +28,11 @@ package java.util;
  * @since 1.2
  */
 public interface Iterator<E> {
-
     /**
      * Returns {@code true} if the iteration has more elements.
      * (In other words, returns {@code true} if {@link #next} would
      * return an element rather than throwing an exception.)
-     * 如果迭代中还有元素，则返回{@code true}。
+     * 如果迭代中还有元素，则返回true。
      *
      * @return {@code true} if the iteration has more elements
      */
@@ -55,10 +54,10 @@ public interface Iterator<E> {
      * is unspecified if the underlying collection is modified while the
      * iteration is in progress in any way other than by calling this
      * method.
-     * 从底层集合中移除此迭代器返回的最后一个元素（可选操作）。
+     * 从底层集合中移除此迭代器返回的最后一个元素。(可选操作)
      *
      * @throws UnsupportedOperationException if the {@code remove}
-     *         operation is not supported by this iterator (不支持操作异常)
+     *         operation is not supported by this iterator 不支持的操作异常
      *
      * @throws IllegalStateException if the {@code next} method has not
      *         yet been called, or the {@code remove} method has already
