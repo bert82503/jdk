b4fd48d5607de5360e90995535dfc782131e6b1e
edwardlee03 <bert825_work@163.com>
2022-04-12 20:08:21 +0800

    improve comment of ReferencePipeline<P_IN, P_OUT> extends AbstractPipeline<P_IN, P_OUT, Stream<P_OUT>>

--- src/main/java/java/util/stream/ReferencePipeline.java
+++ src/main/java/java/util/stream/ReferencePipeline.java
@@ -1,27 +1,4 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
+
 package java.util.stream;
 
 import java.util.Comparator;
@@ -48,9 +25,12 @@ import java.util.function.ToLongFunction;
 /**
  * Abstract base class for an intermediate pipeline stage or pipeline source
  * stage implementing whose elements are of type {@code U}.
+ * 元素类型为U的中间数据流管道阶段或管道源阶段实现的抽象基类。
  *
  * @param <P_IN> type of elements in the upstream source
+ *              上游数据源中元素的类型
  * @param <P_OUT> type of elements in produced by this stage
+ *               在这一阶段产生的元素类型
  *
  * @since 1.8
  */
@@ -60,6 +40,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     /**
      * Constructor for the head of a stream pipeline.
+     * 数据流管道的头节点的构造函数。
      *
      * @param source {@code Supplier<Spliterator>} describing the stream source
      * @param sourceFlags the source flags for the stream source, described in
@@ -73,6 +54,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     /**
      * Constructor for the head of a stream pipeline.
+     * 数据流管道的头节点的构造函数。
      *
      * @param source {@code Spliterator} describing the stream source
      * @param sourceFlags The source flags for the stream source, described in
@@ -87,8 +69,10 @@ abstract class ReferencePipeline<P_IN, P_OUT>
     /**
      * Constructor for appending an intermediate operation onto an existing
      * pipeline.
+     * 将中间操作追加到现有流水线的构造函数。
      *
      * @param upstream the upstream element source.
+     *                 上游元素数据源
      */
     ReferencePipeline(AbstractPipeline<?, P_IN, ?> upstream, int opFlags) {
         super(upstream, opFlags);
@@ -98,6 +82,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     @Override
     final StreamShape getOutputShape() {
+        // 对象引用
         return StreamShape.REFERENCE;
     }
 
@@ -106,6 +91,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                                         Spliterator<P_IN> spliterator,
                                         boolean flattenTree,
                                         IntFunction<P_OUT[]> generator) {
+        // 将数据流管道输出的元素收集到保存这个形状元素的节点中
         return Nodes.collect(helper, spliterator, flattenTree, generator);
     }
 
@@ -113,11 +99,13 @@ abstract class ReferencePipeline<P_IN, P_OUT>
     final <P_IN> Spliterator<P_OUT> wrap(PipelineHelper<P_OUT> ph,
                                      Supplier<Spliterator<P_IN>> supplier,
                                      boolean isParallel) {
+        // 创建一个拆分器，它包装了一个数据源拆分器，与这个数据流形状兼容，以及与流水线助手相关的操作
         return new StreamSpliterators.WrappingSpliterator<>(ph, supplier, isParallel);
     }
 
     @Override
     final Spliterator<P_OUT> lazySpliterator(Supplier<? extends Spliterator<P_OUT>> supplier) {
+        // 延迟拆分器
         return new StreamSpliterators.DelegatingSpliterator<>(supplier);
     }
 
@@ -133,21 +121,27 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
 
     // BaseStream
+    // 数据流的基本接口
 
     @Override
     public final Iterator<P_OUT> iterator() {
+        // 拆分器的迭代器
         return Spliterators.iterator(spliterator());
     }
 
 
     // Stream
+    // 数据流
 
     // Stateless intermediate operations from Stream
+    // 来自数据流的无状态的中间操作
 
     @Override
     public Stream<P_OUT> unordered() {
-        if (!isOrdered())
+        if (!isOrdered()) {
             return this;
+        }
+        // 无状态的操作
         return new StatelessOp<P_OUT, P_OUT>(this, StreamShape.REFERENCE, StreamOpFlag.NOT_ORDERED) {
             @Override
             Sink<P_OUT> opWrapSink(int flags, Sink<P_OUT> sink) {
@@ -155,6 +149,9 @@ abstract class ReferencePipeline<P_IN, P_OUT>
             }
         };
     }
+
+    // 谓词函数-Predicate
+    // 过滤层
 
     @Override
     public final Stream<P_OUT> filter(Predicate<? super P_OUT> predicate) {
@@ -171,13 +168,17 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
                     @Override
                     public void accept(P_OUT u) {
-                        if (predicate.test(u))
+                        if (predicate.test(u)) {
+                            // 谓词函数为真，下游数据流接收
                             downstream.accept(u);
+                        }
                     }
                 };
             }
         };
     }
+
+    // 类型映射函数-Function
 
     @Override
     @SuppressWarnings("unchecked")
@@ -190,6 +191,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                 return new Sink.ChainedReference<P_OUT, R>(sink) {
                     @Override
                     public void accept(P_OUT u) {
+                        // 下游数据流接收类型映射函数的结果
                         downstream.accept(mapper.apply(u));
                     }
                 };
@@ -248,6 +250,8 @@ abstract class ReferencePipeline<P_IN, P_OUT>
         };
     }
 
+    // 降维类型映射
+
     @Override
     public final <R> Stream<R> flatMap(Function<? super P_OUT, ? extends Stream<? extends R>> mapper) {
         Objects.requireNonNull(mapper);
@@ -266,8 +270,9 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                     public void accept(P_OUT u) {
                         try (Stream<? extends R> result = mapper.apply(u)) {
                             // We can do better that this too; optimize for depth=0 case and just grab spliterator and forEach it
-                            if (result != null)
+                            if (result != null) {
                                 result.sequential().forEach(downstream);
+                            }
                         }
                     }
                 };
@@ -294,8 +299,9 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                     public void accept(P_OUT u) {
                         try (IntStream result = mapper.apply(u)) {
                             // We can do better that this too; optimize for depth=0 case and just grab spliterator and forEach it
-                            if (result != null)
+                            if (result != null) {
                                 result.sequential().forEach(downstreamAsInt);
+                            }
                         }
                     }
                 };
@@ -322,8 +328,9 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                     public void accept(P_OUT u) {
                         try (DoubleStream result = mapper.apply(u)) {
                             // We can do better that this too; optimize for depth=0 case and just grab spliterator and forEach it
-                            if (result != null)
+                            if (result != null) {
                                 result.sequential().forEach(downstreamAsDouble);
+                            }
                         }
                     }
                 };
@@ -350,8 +357,9 @@ abstract class ReferencePipeline<P_IN, P_OUT>
                     public void accept(P_OUT u) {
                         try (LongStream result = mapper.apply(u)) {
                             // We can do better that this too; optimize for depth=0 case and just grab spliterator and forEach it
-                            if (result != null)
+                            if (result != null) {
                                 result.sequential().forEach(downstreamAsLong);
+                            }
                         }
                     }
                 };
@@ -378,14 +386,17 @@ abstract class ReferencePipeline<P_IN, P_OUT>
     }
 
     // Stateful intermediate operations from Stream
+    // 来自数据流的有状态的中间操作
 
     @Override
     public final Stream<P_OUT> distinct() {
+        // 去重
         return DistinctOps.makeRef(this);
     }
 
     @Override
     public final Stream<P_OUT> sorted() {
+        // 排序
         return SortedOps.makeRef(this);
     }
 
@@ -396,22 +407,26 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     @Override
     public final Stream<P_OUT> limit(long maxSize) {
-        if (maxSize < 0)
+        if (maxSize < 0) {
             throw new IllegalArgumentException(Long.toString(maxSize));
+        }
         return SliceOps.makeRef(this, 0, maxSize);
     }
 
     @Override
     public final Stream<P_OUT> skip(long n) {
-        if (n < 0)
+        if (n < 0) {
             throw new IllegalArgumentException(Long.toString(n));
-        if (n == 0)
+        }
+        if (n == 0) {
             return this;
-        else
+        } else {
             return SliceOps.makeRef(this, n, -1);
+        }
     }
 
     // Terminal operations from Stream
+    // 来自数据流的终结操作
 
     @Override
     public void forEach(Consumer<? super P_OUT> action) {
@@ -469,6 +484,8 @@ abstract class ReferencePipeline<P_IN, P_OUT>
         return evaluate(FindOps.makeRef(false));
     }
 
+    // 归约操作
+
     @Override
     public final P_OUT reduce(final P_OUT identity, final BinaryOperator<P_OUT> accumulator) {
         return evaluate(ReduceOps.makeRef(identity, accumulator, accumulator));
@@ -483,6 +500,8 @@ abstract class ReferencePipeline<P_IN, P_OUT>
     public final <R> R reduce(R identity, BiFunction<R, ? super P_OUT, R> accumulator, BinaryOperator<R> combiner) {
         return evaluate(ReduceOps.makeRef(identity, accumulator, combiner));
     }
+
+    // 收集器
 
     @Override
     @SuppressWarnings("unchecked")
@@ -531,9 +550,12 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     /**
      * Source stage of a ReferencePipeline.
+     * 引用流水线的数据源阶段。
      *
      * @param <E_IN> type of elements in the upstream source
+     *              上游数据源中元素的类型
      * @param <E_OUT> type of elements in produced by this stage
+     *               在这一阶段产生的元素类型
      * @since 1.8
      */
     static class Head<E_IN, E_OUT> extends ReferencePipeline<E_IN, E_OUT> {
@@ -597,9 +619,12 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     /**
      * Base class for a stateless intermediate stage of a Stream.
+     * 数据流的无状态中间阶段的基类。
      *
      * @param <E_IN> type of elements in the upstream source
+     *              上游数据源中元素的类型
      * @param <E_OUT> type of elements in produced by this stage
+     *               在这一阶段产生的元素类型
      * @since 1.8
      */
     abstract static class StatelessOp<E_IN, E_OUT>
@@ -627,9 +652,12 @@ abstract class ReferencePipeline<P_IN, P_OUT>
 
     /**
      * Base class for a stateful intermediate stage of a Stream.
+     * 数据流的有状态中间阶段的基类。
      *
      * @param <E_IN> type of elements in the upstream source
+     *              上游数据源中元素的类型
      * @param <E_OUT> type of elements in produced by this stage
+     *               在这一阶段产生的元素类型
      * @since 1.8
      */
     abstract static class StatefulOp<E_IN, E_OUT>
